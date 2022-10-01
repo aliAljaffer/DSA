f384983bfdd23dd02b7ef7b27a69b5d5cb4bdad1
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2022-10-01 13:57:20 -0400

    Finalized

--- Project 3/project3.cpp
+++ Project 3/project3.cpp
@@ -1,43 +1,61 @@
-#include "sequence.h"
-#include <iostream>
-#include <string>
+// #include "sequence.h"
+// #include <iostream>
+// #include <string>
 
-void MemTest();
-int main()
-{
-   Sequence mySequence(3);
-   mySequence[0] = 5;
-   cout << mySequence << endl;
-   mySequence.insert(1, 3);
-   MemTest();
-   cout << "Check now";
-   // mySequence.insert(3, 100);
-   // mySequence.insert(5, 200);
-   // mySequence.push_back(50);
-   // // mySequence.clear();
-   // // mySequence.traverser();
-   // // cout << mySequence << endl;
-   // // mySequence.pop_back();
-   // mySequence.push_back(5);
-   // cout << mySequence << endl;
+// void MemTest();
+// int main()
+// {
+//    // Sequence mySequence(3);
+//    // mySequence[0] = 5;
+//    // cout << mySequence << endl;
+//    // mySequence.insert(1, 3);
+//    // MemTest();
+//    // cout << "Check now";
+//    Sequence data1(5);
+//    Sequence data2(0);
 
-   // Sequence secondSequence;
-   // secondSequence = mySequence;
-   // cout << secondSequence << endl;
+//    for (int i = 0; i < 5; i++)
+//    {
+//       data1[i] = (i + 1) * 100;
+//    }
 
-   // secondSequence.erase(0, secondSequence.size());
-   // cout << secondSequence << endl;
-   // secondSequence.push_back(5);
-   // cout << secondSequence << endl;
-   // mySequence.traverser();
-}
+//    data2 = data1;
 
-void MemTest()
-{
-   Sequence s(500000);
-   for (int i = 0; i < 500000; i++)
-   {
-      s[i] = i;
-   }
-   cout << "Check memory" << endl;
-}
+//    data2[0] = 1;
+//    data2[1] = 2;
+
+//    cout << "Data1:      " << data1 << endl;
+//    cout << "Data2:      " << data2 << endl;
+//    cout << "Should be:  <100, 200, 300, 400, 500>" << endl;
+//    cout << "            <1, 2, 300, 400, 500>" << endl
+//         << endl;
+//    // mySequence.insert(3, 100);
+//    // mySequence.insert(5, 200);
+//    // mySequence.push_back(50);
+//    // // mySequence.clear();
+//    // // mySequence.traverser();
+//    // // cout << mySequence << endl;
+//    // // mySequence.pop_back();
+//    // mySequence.push_back(5);
+//    // cout << mySequence << endl;
+
+//    // Sequence secondSequence;
+//    // secondSequence = mySequence;
+//    // cout << secondSequence << endl;
+
+//    // secondSequence.erase(0, secondSequence.size());
+//    // cout << secondSequence << endl;
+//    // secondSequence.push_back(5);
+//    // cout << secondSequence << endl;
+//    // mySequence.traverser();
+// }
+
+// void MemTest()
+// {
+//    Sequence s(500000);
+//    for (int i = 0; i < 500000; i++)
+//    {
+//       s[i] = i;
+//    }
+//    cout << "Check memory" << endl;
+// }
--- Project 3/sequence.cpp
+++ Project 3/sequence.cpp
@@ -85,16 +85,8 @@ Sequence::~Sequence()
 /// @return this - Pointer to the new copy of s
 Sequence &Sequence::operator=(const Sequence &s)
 {
-   // Kill everything in *this*
-   // SequenceNode *current = head;
-   // SequenceNode *killNext;
-   // while (current)
-   // {
-   //    killNext = current->next;
-   //    delete current;
-   //    current = killNext;
-   // }
-   erase(0, numElts);
+   if (!empty())
+      erase(0, numElts);
    // Copy from s
    numElts = s.numElts;
    head = nullptr;
@@ -131,6 +123,7 @@ Sequence::value_type &Sequence::operator[](size_type position)
    }
    SequenceNode *current = head;
    int currentPos = 0;
+   // Advance current to position.
    while (currentPos != position)
    {
       current = current->next;
@@ -139,15 +132,19 @@ Sequence::value_type &Sequence::operator[](size_type position)
    return current->elt;
 }
 
+/// @brief Adds a sequence node at the end of the sequence
+/// @param value - The value to be stored in the new sequence node
 void Sequence::push_back(const value_type &value)
 {
    SequenceNode *newSqNode = new SequenceNode;
    newSqNode->elt = value;
+   // If pushing to an empty sequence
    if (empty())
    {
       head = newSqNode;
       newSqNode->prev = nullptr;
    }
+   // Other cases
    else
    {
       tail->next = newSqNode;
@@ -158,16 +155,20 @@ void Sequence::push_back(const value_type &value)
    numElts++;
 }
 
+/// @brief removes the last sequence node in the sequence
 void Sequence::pop_back()
 {
    if (empty())
       throw exception();
    SequenceNode *tailPtr = tail;
+   // save the tail
+   // if we are popping a sequence of one element
    if (numElts == 1)
    {
       head = nullptr;
       tail = nullptr;
    }
+   // any other cases
    else
    {
       tail = tail->prev;
@@ -177,12 +178,17 @@ void Sequence::pop_back()
    delete tailPtr;
 }
 
+/// @brief Inserts a new sequence node at the given position. Can only insert inbetween nodes.
+/// @param position the position where the new node should be at
+/// @param value the value stored in the new node
 void Sequence::insert(size_type position, value_type value)
 {
+   // Rejected cases
    if (position < 0 || position > numElts - 1)
    {
       throw exception();
    }
+   // If inserting on position 0 => the head.
    else if (position == 0)
    {
       SequenceNode *nodeToAdd = new SequenceNode;
@@ -191,6 +197,7 @@ void Sequence::insert(size_type position, value_type value)
       nodeToAdd->prev = nullptr;
       head = nodeToAdd;
    }
+   // Any other inserts
    else
    {
       SequenceNode *current = head;
@@ -210,6 +217,8 @@ void Sequence::insert(size_type position, value_type value)
    numElts++;
 }
 
+/// @brief get the front of the sequence -- the first node's value
+/// @return the element stored in the head
 const Sequence::value_type &Sequence::front() const
 {
    if (empty())
@@ -217,6 +226,8 @@ const Sequence::value_type &Sequence::front() const
    return head->elt;
 }
 
+/// @brief get the back of the sequence -- the last node's value
+/// @return the element stored in the tail
 const Sequence::value_type &Sequence::back() const
 {
    if (empty())
@@ -224,37 +235,51 @@ const Sequence::value_type &Sequence::back() const
    return tail->elt;
 }
 
+/// @brief checks if the sequence is empty or not
+/// @return True if empty, false if it has nodes.
 bool Sequence::empty() const
 {
    return head == nullptr;
 }
 
+/// @brief gets the current size of the sequence
+/// @return the number of nodes in the sequence
 Sequence::size_type Sequence::size() const
 {
    return numElts;
 }
 
+/// @brief resets the sequence to an empty state.
 void Sequence::clear()
 {
+   // Straightforward. Uses erase on the entire sequence
    if (empty())
       return;
    erase(0, numElts);
 }
 
+/// @brief erases count number of nodes, starting from the index position
+/// @param position the position the erasure should start from
+/// @param count the number of nodes to erase
 void Sequence::erase(size_type position, size_type count)
 {
+   // This one got messy so I don't want to restructure it and risk messing it up even more
    int currNumElts = numElts;
+   // Cases when unable to erase
    if (position + count - 1 > numElts || position < 0 || position > numElts - 1 || count <= 0 || empty())
       throw exception();
 
+   // Keeping track of current position and number of nodes killed.
    int currentPos = 0;
    int numKilled = 0;
    SequenceNode *current = head;
+   // Advance current to position
    while (currentPos != position)
    {
       current = current->next;
       currentPos++;
    }
+   // saving the previous node in order to re-link its next pointer and adjust the previous of the node we end up with
    SequenceNode *savePrevious = (position != 0) ? current->prev : nullptr;
    SequenceNode *nextToKill;
    while (numKilled != count)
@@ -265,29 +290,34 @@ void Sequence::erase(size_type position, size_type count)
       current = nextToKill;
       numKilled++;
    }
+   // Case where every single element was deleted.
    if (position == 0 && count == currNumElts)
    {
       head = nullptr;
       tail = nullptr;
       return;
    }
+   // case when we start from 0. gotta fix the head!
    else if (position == 0)
    {
       head = current;
       head->prev = nullptr;
    }
+   // case if current is null. meaning we need a new tail
    else if (current == nullptr)
    {
       current = savePrevious;
       current->next = nullptr;
       tail = current;
    }
+   // this covers the other cases
    else
    {
       current->prev = savePrevious;
       savePrevious->next = current;
    }
 
+   // final adjustments
    current = head;
    while (current->next)
    {
@@ -295,20 +325,26 @@ void Sequence::erase(size_type position, size_type count)
    }
    tail = current;
 }
+/// @brief Overloaded << operator that allows the sequence to be sent to an ostream
+/// @param os the output stream to send the strings to
+/// @param s the sequence to be printed
+/// @return the output stream - to allow chaining of outputs
 ostream &operator<<(ostream &os, Sequence &s)
 {
+   // Case when s is empty.
    if (s.empty())
    {
       os << "The sequence is empty." << endl;
       return os;
    }
    int currentPos = 0;
-   os << "[";
+   os << "<";
+   // Loop on every node and print it.
    while (currentPos < s.size())
    {
       if (currentPos == (s.size() - 1)) // When at last index, end with ]
       {
-         os << s[currentPos] << "]" << endl;
+         os << s[currentPos] << ">" << endl;
          break;
       }
       os << s[currentPos] << ", ";
@@ -316,7 +352,9 @@ ostream &operator<<(ostream &os, Sequence &s)
    }
    return os;
 }
-// Testing connections. If head can reach tail and vice versa. Tests nexts and prevs.
+
+/// @brief A methood to test connections. If head can reach tail and vice versa. Tests nexts and prevs.
+/// @return true if reached head->tail and then tail->head. false if failed somewhere.
 bool Sequence::traverser()
 {
    if (empty())
