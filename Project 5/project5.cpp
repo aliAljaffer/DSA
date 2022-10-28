d1cfbadf94f982918b295fe7f7668c949886b16e
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2022-10-28 09:38:47 -0400

    Update project5.cpp

--- Project 5/project5.cpp
+++ Project 5/project5.cpp
@@ -7,32 +7,42 @@
 void fileReader(string, Trie *);
 int main()
 {
-   Trie *tr = new Trie;
-   fileReader("words.txt", tr);
+   Trie tr;
+   tr.insert("five");
+   tr.insert("fire");
+   tr.insert("free");
    vector<string> result;
-   string userWord;
-   string userAnswer;
-   while (1)
+   tr.autocomplete("fi", result);
+   for (auto x : result)
    {
-      cout << "Enter a word you'd like to autocomplete (enter \"byebye\" to terminate)" << endl;
-      cin >> userWord;
-      if (userWord == "byebye")
-         return 0;
-      tr->autocomplete(userWord, result);
-      int numWords = result.size();
-      cout << "There are " << to_string(numWords) << " words that start with " << userWord << endl;
-      cout << "Would you like to display them?" << endl;
-      cin >> userAnswer;
-      if (userAnswer == "byebye")
-         return 0;
-      if (userAnswer[0] == 'Y' || userAnswer[0] == 'y')
-      {
-         for (auto x : result)
-         {
-            cout << "\t" + x << endl;
-         }
-      }
+      cout << "\t" + x << endl;
    }
+   // Trie *tr = new Trie;
+   // fileReader("words.txt", tr);
+   // vector<string> result;
+   // string userWord;
+   // string userAnswer;
+   // while (1)
+   // {
+   //    cout << "Enter a word you'd like to autocomplete (enter \"byebye\" to terminate)" << endl;
+   //    cin >> userWord;
+   //    if (userWord == "byebye")
+   //       return 0;
+   //    tr->autocomplete(userWord, result);
+   //    int numWords = result.size();
+   //    cout << "There are " << to_string(numWords) << " words that start with " << userWord << endl;
+   //    cout << "Would you like to display them?" << endl;
+   //    cin >> userAnswer;
+   //    if (userAnswer == "byebye")
+   //       return 0;
+   //    if (userAnswer[0] == 'Y' || userAnswer[0] == 'y')
+   //    {
+   //       for (auto x : result)
+   //       {
+   //          cout << "\t" + x << endl;
+   //       }
+   //    }
+   // }
 }
 
 void fileReader(string filename, Trie *myTrie)
